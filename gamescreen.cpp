379c5f5988e9225399e956b7836b199b5ca30318
Andrew <akabmm@mail.umkc.edu>
2021-04-20 06:32:12 -0500

    Finished Place a Piece
    Place a piece (basic) works. Buttons that fill with alternate player color.

--- gamescreen.cpp
+++ gamescreen.cpp
@@ -1,6 +1,7 @@
 #include "gamescreen.h"
 #include "ui_gamescreen.h"
-#include <QDrag>
+#include <QPushButton>
+#include <QDropEvent>
 
 gamescreen::gamescreen(QWidget *parent) :
     QDialog(parent),
@@ -8,9 +9,394 @@ gamescreen::gamescreen(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    //connect(ui->space1, SIGNAL(clicked()), this, SLOT(on_space1_clicked()));
 }
 
 gamescreen::~gamescreen()
 {
     delete ui;
+}
+
+void gamescreen::on_space1_clicked()
+{
+    if ((turnTracker % 2) == 0)
+    {
+    ui->space1->setStyleSheet("background-color: gray; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else if ((turnTracker % 2) == 1)
+    {
+    ui->space1->setStyleSheet("background-color: black; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else{
+        turnTracker = turnTracker + 0;
+    }
+    turnTracker++;
+}
+
+void gamescreen::on_space2_clicked()
+{
+    if ((turnTracker % 2) == 0)
+    {
+    ui->space2->setStyleSheet("background-color: gray; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else if ((turnTracker % 2) == 1)
+    {
+    ui->space2->setStyleSheet("background-color: black; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else{
+        turnTracker = turnTracker + 0;
+    }
+    turnTracker++;
+}
+
+void gamescreen::on_space3_clicked()
+{
+    if ((turnTracker % 2) == 0)
+    {
+    ui->space3->setStyleSheet("background-color: gray; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else if ((turnTracker % 2) == 1)
+    {
+    ui->space3->setStyleSheet("background-color: black; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else{
+        turnTracker = turnTracker + 0;
+    }
+    turnTracker++;
+}
+
+void gamescreen::on_space4_clicked()
+{
+    if ((turnTracker % 2) == 0)
+    {
+    ui->space4->setStyleSheet("background-color: gray; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else if ((turnTracker % 2) == 1)
+    {
+    ui->space4->setStyleSheet("background-color: black; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else{
+        turnTracker = turnTracker + 0;
+    }
+    turnTracker++;
+}
+
+void gamescreen::on_space5_clicked()
+{
+    if ((turnTracker % 2) == 0)
+    {
+    ui->space5->setStyleSheet("background-color: gray; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else if ((turnTracker % 2) == 1)
+    {
+    ui->space5->setStyleSheet("background-color: black; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else{
+        turnTracker = turnTracker + 0;
+    }
+    turnTracker++;
+}
+
+void gamescreen::on_space6_clicked()
+{
+    if ((turnTracker % 2) == 0)
+    {
+    ui->space6->setStyleSheet("background-color: gray; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else if ((turnTracker % 2) == 1)
+    {
+    ui->space6->setStyleSheet("background-color: black; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else{
+        turnTracker = turnTracker + 0;
+    }
+    turnTracker++;
+}
+
+void gamescreen::on_space7_clicked()
+{
+    if ((turnTracker % 2) == 0)
+    {
+    ui->space7->setStyleSheet("background-color: gray; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else if ((turnTracker % 2) == 1)
+    {
+    ui->space7->setStyleSheet("background-color: black; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else{
+        turnTracker = turnTracker + 0;
+    }
+    turnTracker++;
+}
+
+void gamescreen::on_space8_clicked()
+{
+    if ((turnTracker % 2) == 0)
+    {
+    ui->space8->setStyleSheet("background-color: gray; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else if ((turnTracker % 2) == 1)
+    {
+    ui->space8->setStyleSheet("background-color: black; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else{
+        turnTracker = turnTracker + 0;
+    }
+    turnTracker++;
+}
+
+void gamescreen::on_space9_clicked()
+{
+    if ((turnTracker % 2) == 0)
+    {
+    ui->space9->setStyleSheet("background-color: gray; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else if ((turnTracker % 2) == 1)
+    {
+    ui->space9->setStyleSheet("background-color: black; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else{
+        turnTracker = turnTracker + 0;
+    }
+    turnTracker++;
+}
+
+void gamescreen::on_space10_clicked()
+{
+    if ((turnTracker % 2) == 0)
+    {
+    ui->space10->setStyleSheet("background-color: gray; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else if ((turnTracker % 2) == 1)
+    {
+    ui->space10->setStyleSheet("background-color: black; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else{
+        turnTracker = turnTracker + 0;
+    }
+    turnTracker++;
+}
+
+void gamescreen::on_space11_clicked()
+{
+    if ((turnTracker % 2) == 0)
+    {
+    ui->space11->setStyleSheet("background-color: gray; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else if ((turnTracker % 2) == 1)
+    {
+    ui->space11->setStyleSheet("background-color: black; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else{
+        turnTracker = turnTracker + 0;
+    }
+    turnTracker++;
+}
+
+void gamescreen::on_space12_clicked()
+{
+    if ((turnTracker % 2) == 0)
+    {
+    ui->space12->setStyleSheet("background-color: gray; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else if ((turnTracker % 2) == 1)
+    {
+    ui->space12->setStyleSheet("background-color: black; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else{
+        turnTracker = turnTracker + 0;
+    }
+    turnTracker++;
+}
+
+void gamescreen::on_space13_clicked()
+{
+    if ((turnTracker % 2) == 0)
+    {
+    ui->space13->setStyleSheet("background-color: gray; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else if ((turnTracker % 2) == 1)
+    {
+    ui->space13->setStyleSheet("background-color: black; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else{
+        turnTracker = turnTracker + 0;
+    }
+    turnTracker++;
+}
+
+void gamescreen::on_space14_clicked()
+{
+    if ((turnTracker % 2) == 0)
+    {
+    ui->space14->setStyleSheet("background-color: gray; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else if ((turnTracker % 2) == 1)
+    {
+    ui->space14->setStyleSheet("background-color: black; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else{
+        turnTracker = turnTracker + 0;
+    }
+    turnTracker++;
+}
+
+void gamescreen::on_space15_clicked()
+{
+    if ((turnTracker % 2) == 0)
+    {
+    ui->space15->setStyleSheet("background-color: gray; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else if ((turnTracker % 2) == 1)
+    {
+    ui->space15->setStyleSheet("background-color: black; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else{
+        turnTracker = turnTracker + 0;
+    }
+    turnTracker++;
+}
+
+void gamescreen::on_space16_clicked()
+{
+    if ((turnTracker % 2) == 0)
+    {
+    ui->space16->setStyleSheet("background-color: gray; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else if ((turnTracker % 2) == 1)
+    {
+    ui->space16->setStyleSheet("background-color: black; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else{
+        turnTracker = turnTracker + 0;
+    }
+    turnTracker++;
+}
+
+void gamescreen::on_space17_clicked()
+{
+    if ((turnTracker % 2) == 0)
+    {
+    ui->space17->setStyleSheet("background-color: gray; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else if ((turnTracker % 2) == 1)
+    {
+    ui->space17->setStyleSheet("background-color: black; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else{
+        turnTracker = turnTracker + 0;
+    }
+    turnTracker++;
+}
+
+void gamescreen::on_space18_clicked()
+{
+    if ((turnTracker % 2) == 0)
+    {
+    ui->space18->setStyleSheet("background-color: gray; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else if ((turnTracker % 2) == 1)
+    {
+    ui->space18->setStyleSheet("background-color: black; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else{
+        turnTracker = turnTracker + 0;
+    }
+    turnTracker++;
+}
+
+void gamescreen::on_space19_clicked()
+{
+    if ((turnTracker % 2) == 0)
+    {
+    ui->space19->setStyleSheet("background-color: gray; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else if ((turnTracker % 2) == 1)
+    {
+    ui->space19->setStyleSheet("background-color: black; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else{
+        turnTracker = turnTracker + 0;
+    }
+    turnTracker++;
+}
+
+void gamescreen::on_space20_clicked()
+{
+    if ((turnTracker % 2) == 0)
+    {
+    ui->space20->setStyleSheet("background-color: gray; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else if ((turnTracker % 2) == 1)
+    {
+    ui->space20->setStyleSheet("background-color: black; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else{
+        turnTracker = turnTracker + 0;
+    }
+    turnTracker++;
+}
+
+void gamescreen::on_space21_clicked()
+{
+    if ((turnTracker % 2) == 0)
+    {
+    ui->space21->setStyleSheet("background-color: gray; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else if ((turnTracker % 2) == 1)
+    {
+    ui->space21->setStyleSheet("background-color: black; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else{
+        turnTracker = turnTracker + 0;
+    }
+    turnTracker++;
+}
+
+void gamescreen::on_space22_clicked()
+{
+    if ((turnTracker % 2) == 0)
+    {
+    ui->space22->setStyleSheet("background-color: gray; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else if ((turnTracker % 2) == 1)
+    {
+    ui->space22->setStyleSheet("background-color: black; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else{
+        turnTracker = turnTracker + 0;
+    }
+    turnTracker++;
+}
+
+void gamescreen::on_space23_clicked()
+{
+    if ((turnTracker % 2) == 0)
+    {
+    ui->space23->setStyleSheet("background-color: gray; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else if ((turnTracker % 2) == 1)
+    {
+    ui->space23->setStyleSheet("background-color: black; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else{
+        turnTracker = turnTracker + 0;
+    }
+    turnTracker++;
+}
+
+void gamescreen::on_space24_clicked()
+{
+    if ((turnTracker % 2) == 0)
+    {
+    ui->space24->setStyleSheet("background-color: gray; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else if ((turnTracker % 2) == 1)
+    {
+    ui->space24->setStyleSheet("background-color: black; border-style: solid; border-width: 1px; border-radius: 10px; border-color: black; max-width: 20px; max-height: 20px; min-width :20px; min-height: 20px;");
+    }
+    else{
+        turnTracker = turnTracker + 0;
+    }
+    turnTracker++;
 }
--- gamescreen.h
+++ gamescreen.h
@@ -13,7 +13,59 @@ class gamescreen : public QDialog
 
 public:
     explicit gamescreen(QWidget *parent = nullptr);
+    int turnTracker = 0;
     ~gamescreen();
+
+private slots:
+
+
+    void on_space1_clicked();
+
+    void on_space2_clicked();
+
+    void on_space3_clicked();
+
+    void on_space4_clicked();
+
+    void on_space5_clicked();
+
+    void on_space6_clicked();
+
+    void on_space7_clicked();
+
+    void on_space8_clicked();
+
+    void on_space9_clicked();
+
+    void on_space10_clicked();
+
+    void on_space11_clicked();
+
+    void on_space12_clicked();
+
+    void on_space13_clicked();
+
+    void on_space14_clicked();
+
+    void on_space15_clicked();
+
+    void on_space16_clicked();
+
+    void on_space17_clicked();
+
+    void on_space18_clicked();
+
+    void on_space19_clicked();
+
+    void on_space20_clicked();
+
+    void on_space21_clicked();
+
+    void on_space22_clicked();
+
+    void on_space23_clicked();
+
+    void on_space24_clicked();
 
 private:
     Ui::gamescreen *ui;
